b568a32ea03145e98f0bd33517ae83cee1da8e67
Joshua <navyjosh@gmail.com>
2020-05-05 12:29:21 -0400

    build database script

--- epicov/management/commands/build_db.py
+++ epicov/management/commands/build_db.py
@@ -14,31 +14,34 @@ class Command(BaseCommand):
            'command.'
 
     def handle(self, *args, **kwargs):
-        data_dir = os.path.join(BASE_DIR, 'raw')
-        files = os.listdir(data_dir)
-        meta_data_url = "https://github.com/nextstrain/ncov/blob/master/data/metadata.tsv?raw=true"
-        fn = os.path.join(data_dir, 'metadata.tsv')
-        urllib.request.urlretrieve(meta_data_url, fn)
-        metadata = pd.read_csv(fn, delimiter='\t')
-        fn = None
-        for file in files:
-            if ".fasta" in file:
-                fn = file
-                break
-        if fn:
-            engine = create_engine('postgresql://covid:%s@localhost:5432/covid' % quote_plus('nutrit1on+'))
-            with open(fn, 'r') as fh:
-                missing_epi = []
-                for record in SeqIO.parse(fh, "fasta"):
-                    try:
-                        gisaid_epi_isl = record.id.split("|")[-2]
-                        record_id = record.id
-                        seq = str(record.seq)
-                        pd.Series({'ident': record_id, 'seq': seq}).append(metadata.loc[metadata.gisaid_epi_isl == gisaid_epi_isl]).to_sql('covid', engine, if_exists='fail')
-                        self.stdout.write(self.style.SUCCESS('Wrote Record ID: %s to database'))
+        try:
+            data_dir = os.path.join(BASE_DIR, 'raw')
+            files = os.listdir(data_dir)
+            meta_data_url = "https://github.com/nextstrain/ncov/blob/master/data/metadata.tsv?raw=true"
+            fn = os.path.join(data_dir, 'metadata.tsv')
+            urllib.request.urlretrieve(meta_data_url, fn)
+            metadata = pd.read_csv(fn, delimiter='\t')
+            fn = None
+            for file in files:
+                if ".fasta" in file:
+                    fn = file
+                    break
+            if fn:
+                engine = create_engine('postgresql://covid:%s@localhost:5432/covid' % quote_plus('nutrit1on+'))
+                with open(fn, 'r') as fh:
+                    missing_epi = []
+                    for record in SeqIO.parse(fh, "fasta"):
+                        try:
+                            gisaid_epi_isl = record.id.split("|")[-2]
+                            record_id = record.id
+                            seq = str(record.seq)
+                            pd.Series({'ident': record_id, 'seq': seq}).append(metadata.loc[metadata.gisaid_epi_isl == gisaid_epi_isl]).to_sql('covid', engine, if_exists='fail')
+                            self.stdout.write('Wrote Record ID: %s to database')
 
-                    except Exception as e:
-                        missing_epi.append(record.id)
+                        except Exception as e:
+                            self.stdout.write(str(e))
+        except Exception as e:
+            self.stdout.write(str(e))
 
 
             df = pd.DataFrame
